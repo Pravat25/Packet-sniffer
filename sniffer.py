f08a5fa9cf58af8dbdb08d03d8a4e90d9315f639
Pravat25 <131624952+Pravat25@users.noreply.github.com>
2026-03-19 00:14:00 +1100

    upgraded packet sniffer with protocol and port detection

--- sniffer.py
+++ sniffer.py
@@ -1,14 +1,37 @@
-from scapy.all import sniff
+from scapy.all import sniff, IP, TCP, UDP, ICMP
+
+packet_count = 0
 
 def packet_callback(packet):
+    global packet_count
+    packet_count += 1
 
-    if packet.haslayer("IP"):
+    if packet.haslayer(IP):
+        src = packet[IP].src
+        dst = packet[IP].dst
 
-        src = packet["IP"].src
-        dst = packet["IP"].dst
+        protocol = ""
 
-        print(f"Packet: {src} -> {dst}")
+        if packet.haslayer(TCP):
+            protocol = "TCP"
+            sport = packet[TCP].sport
+            dport = packet[TCP].dport
+            print(f"[{packet_count}] {protocol} {src}:{sport} -> {dst}:{dport}")
 
-print("Starting packet sniffer...\n")
+        elif packet.haslayer(UDP):
+            protocol = "UDP"
+            sport = packet[UDP].sport
+            dport = packet[UDP].dport
+            print(f"[{packet_count}] {protocol} {src}:{sport} -> {dst}:{dport}")
 
-sniff(prn=packet_callback, count=10)
+        elif packet.haslayer(ICMP):
+            protocol = "ICMP"
+            print(f"[{packet_count}] {protocol} {src} -> {dst}")
+
+        else:
+            print(f"[{packet_count}] Other {src} -> {dst}")
+
+
+print("Starting advanced packet sniffer...\n")
+
+sniff(prn=packet_callback, count=20)
